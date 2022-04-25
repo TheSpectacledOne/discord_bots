1d556bf676bef46e6dd98efa63d1db189176b12f
George L. Malone <georgemalone@outlook.com.au>
2022-04-25 19:48:47 +0800

    Rearrange meme_machine.py
    * Add section-break lines
    
    * Set EOL to Unix
    
    * Make max_meme_size screaming snake case (because it's a constant)
    
    * Add main function definition

--- meme_machine.py
+++ meme_machine.py
@@ -1,31 +1,36 @@
-import os, sys
+# Module imports
+# -----------------------------------------------------------------------------
+
+
 import discord
+import os
+import sys
+
 from discord.ext import commands
 
-# 8MB max
-max_meme_size = 8*1024*1024
 
-def read_token(file_path: str) -> str:
-    with open(file_path, "r") as fh:
-        data = fh.read()
-    return data.strip()
+# Constants
+# -----------------------------------------------------------------------------
+
+
+# 8MB max
+MAX_MEME_SIZE = 8 * 1024 * 1024
+
+
+# Function definitions
+# -----------------------------------------------------------------------------
+
 
 def get_size(file_path: str):
     return os.path.getsize(file_path)
 
-def under_8MB(cur_size, file_size):
-    global max_meme_size
-    if file_size + cur_size < max_meme_size:
-        return True
-    else:
-        return False
 
 def make_upload_packages(dir_path):
     files_in_dir = [os.path.join(dir_path, file) for file in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, file))]
     upload_packages = [[]]
     cur_size = 0
     index = 0
-    
+
     for file in files_in_dir:
         file_size = get_size(file)
         if not under_8MB(0, file_size):
@@ -43,6 +48,25 @@ def make_upload_packages(dir_path):
         upload_packages = upload_packages[:-1]
 
     return upload_packages
+
+
+def read_token(file_path: str) -> str:
+    with open(file_path, "r") as fh:
+        data = fh.read()
+    return data.strip()
+
+
+def under_8MB(cur_size, file_size):
+    global MAX_MEME_SIZE
+    if file_size + cur_size < MAX_MEME_SIZE:
+        return True
+    else:
+        return False
+
+
+# Bot handling
+# -----------------------------------------------------------------------------
+
 
 bot = commands.Bot(command_prefix='?')
 
@@ -64,9 +88,24 @@ async def meme(ctx, meme_type):
     else:
         await ctx.send("Where are your memes?")
         return
-    
+
     for meme in memes:
         await ctx.send(files=meme)
 
-token = read_token(sys.argv[1]) # Stored in discord_bot_token.txt
-bot.run(token)
+
+# Main function
+# -----------------------------------------------------------------------------
+
+
+def main():
+    global bot
+    token = read_token(sys.argv[1])  # Stored in discord_bot_token.txt
+    bot.run(token)
+
+
+# Entrypoint
+# -----------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    main()
